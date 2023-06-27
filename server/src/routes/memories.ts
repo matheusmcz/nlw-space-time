36f3de3ff7dcdc205f38473d00a8802b47a6e1b0
Matheus Vieira <matheusmczvieira@gmail.com>
2023-06-27 18:02:19 -0300

    chore: Server routes

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -16,12 +16,13 @@ export async function memoriesRoutes(app: FastifyInstance) {
         createdAt: "asc",
       },
     });
-
+    console.log(memories);
     return memories.map((memory) => {
       return {
         id: memory.id,
         coverUrl: memory.coverUrl,
         excerpt: memory.content.substring(0, 115).concat("..."),
+        createdAt: memory.createdAt,
       };
     });
   });
